6bb784e4f03674e2b1a877bd21f8fc7c8870a580
Joseph Quevedo <41499530+dewstend@users.noreply.github.com>
2024-02-15 22:39:38 -0400

    demographic_data_analyzer: add min_work_hours

--- data-analysis-with-python/demographic-data-analyzer/demographic_data_analyzer.py
+++ data-analysis-with-python/demographic-data-analyzer/demographic_data_analyzer.py
@@ -43,7 +43,7 @@ def calculate_demographic_data(print_data=True):
     )
 
     # What is the minimum number of hours a person works per week (hours-per-week feature)?
-    min_work_hours = None
+    min_work_hours = df["hours-per-week"].min()
 
     # What percentage of the people who work the minimum number of hours per week have a salary of >50K?
     num_min_workers = None
