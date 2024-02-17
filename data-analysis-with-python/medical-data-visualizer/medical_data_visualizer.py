ce08b97a4f0067587a580a5d297f9e69f3d312a0
Joseph Quevedo <41499530+dewstend@users.noreply.github.com>
2024-02-17 19:52:37 -0400

    medical_data_visualizer: normalize `cholesterol` and `gluc` between 0 and 1

--- data-analysis-with-python/medical-data-visualizer/medical_data_visualizer.py
+++ data-analysis-with-python/medical-data-visualizer/medical_data_visualizer.py
@@ -4,13 +4,17 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 # Import data
-df = None
+df = pd.read_csv("medical_examination.csv")
 
 # Add 'overweight' column
-df['overweight'] = None
+df["overweight"] = ((df["weight"] / (df["height"] / 100) ** 2) > 25).astype(int)
 
 # Normalize data by making 0 always good and 1 always bad. If the value of 'cholesterol' or 'gluc' is 1, make the value 0. If the value is more than 1, make the value 1.
+df.loc[df["cholesterol"] == 1, "cholesterol"] = 0
+df.loc[df["gluc"] == 1, "gluc"] = 0
 
+df.loc[df["cholesterol"] > 1, "cholesterol"] = 1
+df.loc[df["gluc"] > 1, "gluc"] = 1
 
 # Draw Categorical Plot
 def draw_cat_plot():
